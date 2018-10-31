bd51aac4f497ecf8dfcb07f4194548c2294b0080
brokorenko <brokorenko@gmail.com>
2018-10-31 20:26:54 +0300

    mysql db init added

--- src/main/java/com/andy/recipe/model/UnitOfMeasure.java
+++ src/main/java/com/andy/recipe/model/UnitOfMeasure.java
@@ -8,13 +8,21 @@ public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String description;
 
-    public String getName() {
-        return name;
+    public Long getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
